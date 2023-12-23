4be025dfa28776f38fd277a9e70fefa11423a545
SamsonDm <samsondm97@gmail.com>
2023-12-23 17:23:14 +0300

    feat: solved carbon-dating, count-cats, common-character-count

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  /* Validation */
+  if (!(sampleActivity instanceof String || (typeof sampleActivity === 'string'))) return false;
+  const sampleNumber = Number(sampleActivity);
+  if (!sampleNumber) return false;
+  if (sampleNumber <= 0 || sampleNumber > MODERN_ACTIVITY) return false;
+
+  const k = 0.693 / HALF_LIFE_PERIOD; // Eq 3
+  const date = -Math.log(sampleNumber / MODERN_ACTIVITY) / k; // Eq 8
+  return Math.ceil(date);
 }
 
 module.exports = {
--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,11 +11,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const makeMap = (s) => {
+    const m = new Map();
+    s.split('').forEach((char) => {
+      if (m.has(char)) {
+        m.set(char, m.get(char) + 1);
+      } else {
+        m.set(char, 1);
+      }
+    });
+    return m;
+  };
+  const map1 = makeMap(s1);
+  const map2 = makeMap(s2);
+  let count = 0;
+  map1.forEach((value, char) => {
+    if (map2.has(char)) count += Math.min(map1.get(char), map2.get(char));
+  });
+  return count;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
