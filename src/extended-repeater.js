8138b648d32d3cb048ef66091e4ddd45be0fd724
SamsonDm <samsondm97@gmail.com>
2023-12-24 15:38:04 +0300

    feat: solve extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,24 +2,66 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  // console.log(str);
+  // console.log(options);
+  let separator = '+';
+  if (options.separator !== undefined) {
+    separator = options.separator;
+  }
+
+  let additionSeparator = '|';
+  if (options.additionSeparator !== undefined) {
+    additionSeparator = options.additionSeparator;
+  }
+
+  let addition = '';
+  if (options.addition !== undefined) {
+    addition = `${options.addition}`;
+  }
+
+  let strConverted = '';
+  if (str !== undefined) {
+    strConverted = `${str}`;
+  }
+
+  let repeatTimes = 1;
+  if (options.repeatTimes !== undefined) {
+    repeatTimes = options.repeatTimes;
+  }
+
+  let additionRepeatTimes = 1;
+  if (options.additionRepeatTimes !== undefined) {
+    additionRepeatTimes = options.additionRepeatTimes;
+  }
+
+  const additions = new Array(additionRepeatTimes);
+  additions.fill(addition);
+  const fullAddition = additions.join(additionSeparator);
+
+  const strings = new Array(repeatTimes);
+  strings.fill(strConverted.concat(fullAddition));
+  const fullStr = strings.join(separator);
+  return fullStr;
 }
 
+const myTest = repeater('STRING', {
+  repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00',
+});
+
 module.exports = {
-  repeater
+  repeater,
 };
